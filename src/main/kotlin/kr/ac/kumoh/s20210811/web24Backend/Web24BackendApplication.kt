820d589af572b6d9df1ef286c6e6986f6a0bdf4b
dltkd <dltkdduq9229@gmail.com>
2024-12-13 16:44:56 +0900

    Initial commit

---
+++ src/main/kotlin/kr/ac/kumoh/s20210811/web24Backend/Web24BackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20210811.web24Backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Web24BackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<Web24BackendApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20210811/web24Backend/Web24BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20210811.web24Backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Web24BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
